07977743a70c6bcbfc5a00ef7c13f86b0ab5d181
puddingii <gun4930@naver.com>
2024-03-21 03:41:00 +0900

    chore(index): health-check 함수 작성 및 필요없는 로직 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import AppConfig from './config';
 import { ImageModule } from './modules/image/image.module';
 import { TypedConfigModule, dotenvLoader } from 'nest-typed-config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
 	imports: [
@@ -14,12 +11,9 @@ import { join } from 'path';
 			load: dotenvLoader(),
 			isGlobal: true,
 		}),
-		ServeStaticModule.forRoot({
-			rootPath: join(__dirname, '..', 'assets'),
-		}),
 		ImageModule,
 	],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [],
 })
 export class AppModule {}
