c17b219b5f42b0eefe4b2e137fe4995433345d8c
geckosboy <geckosboy@verywords.com>
2024-10-27 14:23:15 +0900

    feat(cicd): resize, cache cicd 작업, storage 서버 볼륨지정으로 저장한 파일 영구 저장

--- apps/storage/ecosystem.config.js
+++ apps/storage/ecosystem.config.js
@@ -13,20 +13,4 @@ module.exports = [
 			PORT: '3032',
 		},
 	},
-	// {
-	// 	name: 'file_storage_backend_2',
-	// 	script: './apps/storage/dist/apps/storage/src/main.js',
-	// 	wait_ready: true,
-	// 	listen_timeout: 10000,
-	// 	env_production: {
-	// 		NODE_ENV: 'production',
-	// 		PORT: '3132',
-	// 		// PORT: '7612',
-	// 	},
-	// 	env_development: {
-	// 		NODE_ENV: 'development',
-	// 		PORT: '3132',
-	// 		ORIGIN_LIST_STR: 'http://localhost:3044'
-	// 	},
-	// },
 ];
